59c5df66bf2885f8d15ce592656f8410ec0c5a5c
crafter-deo <adesolaadeoluwa@gmail.com>
2022-09-28 18:02:22 +0100

    complete main functionality;

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use std::env;
-
 use clap::Parser;
+use std::fs::File;
+use std::io::Error;
+use std::io::{BufRead, BufReader};
 
 /// Search for a pattern in a file and display the lines that contain it
+#[derive(Parser)]
 struct Cli {
-    /// the pattern to look for
+    /// The pattern to look for
     pattern: String,
-    /// the pathh to search
+    /// The path to search
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    let args = Cli::parse();
+    let f = File::open(&args.path)?;
+    let reader = BufReader::new(f);
+    
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                if line.contains(&args.pattern) {
+                    println!("{}", line)
+                }
+            }
+            Err(err) => panic!("{}", err),
+        };
+    }
+    // let content = std::fs::read_to_string(&args.path)?;
+    
+
+    // println!("file content: {}", content);
+
+    Ok(())
 }
